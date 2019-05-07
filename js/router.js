6c724e66540e6b92bef852bcc3a5dd38e41aa541
Raymond <raymondcheesheng@gmail.com>
2019-05-08 01:03:30 +0800

    ok

---
+++ js/router.js
@@ -0,0 +1,7 @@
+$('#header').load('./shared/header.html');
+$('#headerEmployer').load('./shared/headerEmployer.html');
+$('#footer').load('./shared/footer.html');
+$('#footerFixed').load('./shared/footerFixed.html');
+
+
+
