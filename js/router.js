82b91b299e9d21aaf3d1af9bcaa2f39af9ded1d2
JacksonHiew <nelson145258116@hotmail.com>
2019-07-14 23:29:03 +0800

    ok

--- js/router.js
+++ js/router.js
@@ -1,6 +1,10 @@
 $('#header').load('./shared/header.html');
 $('#headerEmployer').load('./shared/headerEmployer.html');
-$('#footer').load('./shared/footer.html');
+$('#footer').load('./shared/footer.html', function () {
+    if (document.querySelector(".high").clientHeight > (window.innerHeight - 112)) {
+        document.querySelector(".foot").classList.remove("position-fixed");
+    }
+});
 $('#footerFixed').load('./shared/footerFixed.html');
 
 
